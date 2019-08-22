09ef9fc4b4f600f0320657f0a980f373d1feb474
Scott Roberts <sroberts.kia@gmail.com>
2019-08-22 21:02:30 +0000

    Added addProperty function, Inserts a new property into the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -114,7 +114,7 @@ const getAllProperties = function (options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   // 3
@@ -134,7 +134,7 @@ const getAllProperties = function (options, limit = 10) {
       queryString += `AND cost_per_night < $${queryParams.length} `;
     }
   }
-  
+
   queryString += `
   GROUP BY properties.id`;
   if (options.minimum_rating){
@@ -149,7 +149,7 @@ const getAllProperties = function (options, limit = 10) {
   `;
 
   // 5
-  console.log(queryString, queryParams);
+  //console.log(queryString, queryParams);
 
   // 6
   return pool.query(queryString, queryParams)
@@ -164,9 +164,32 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property);
+  const insertParams = Object.values(property);
+  console.log(Object.values(property));
+  console.log("insertparams", insertParams)
+  
+  return pool.query(`
+  INSERT INTO properties (
+  title,
+  description,
+  number_of_bedrooms,
+  number_of_bathrooms,
+  parking_spaces,
+  cost_per_night,
+  thumbnail_photo_url,
+  cover_photo_url,
+  street,
+  country,
+  city,
+  province,
+  post_code,
+  owner_id)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *`, insertParams)
+  .then(res => {
+      console.log(res.rows[0]);
+  })
 }
 exports.addProperty = addProperty;
+
